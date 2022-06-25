42e90ef9a91f650427d5455262a3bb4525bdbcc0
mikecaesario <michaelcaesario@live.com>
2022-06-26 06:39:12 +0700

    Initial Commit

---
+++ Aman Covid Tracker/Aman_Covid_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aman_Covid_TrackerApp.swift
+//  Aman Covid Tracker
+//
+//  Created by Michael Caesario on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Aman_Covid_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
